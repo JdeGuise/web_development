5257704b0a5d14cedb6e4ad251f71bf1cf8e54ab
John <degui1r@cmich.edu>
2016-11-06 13:57:56 -0500

    displays 5 cards and progresses work

--- lab9/js/main.js
+++ lab9/js/main.js
@@ -6,17 +6,17 @@ function initializeDeck(){
 	var diamonds = new Array();
 	var sorted_deck = new Array();
 
-	for(var i = 1; i <= 13; i++){
+	for(var i = 0; i <= 12; i++){
 
 		hearts[i] = new Image();
 		clubs[i] = new Image();
 		spades[i] = new Image();
 		diamonds[i] = new Image();
 
-		hearts[i].src = 'card_assets/'+i+'H.png';
-		clubs[i].src = 'card_assets/'+i+'C.png';
-		spades[i].src = 'card_assets/'+i+'S.png';
-		diamonds[i].src = 'card_assets/'+i+'D.png';
+		hearts[i].src = 'card_assets/'+(i+1)+'H.png';
+		clubs[i].src = 'card_assets/'+(i+1)+'C.png';
+		spades[i].src = 'card_assets/'+(i+1)+'S.png';
+		diamonds[i].src = 'card_assets/'+(i+1)+'D.png';			
 
 	}
 
@@ -25,9 +25,10 @@ function initializeDeck(){
 		sorted_deck = sorted_deck.concat(spades);
 		sorted_deck = sorted_deck.concat(diamonds);
 		
-
 		/* sorted_deck currently has an undefined element in index 0 of each of the suit sets*/
 		shuffleDeck(sorted_deck);
+		console.log(sorted_deck[0]);
+		return sorted_deck;
 }
 
 //parameter
@@ -52,15 +53,47 @@ function shuffleDeck(current_deck){
 		current_deck[i - 1] = current_deck[j];
 		current_deck[j] = x;
 	}
-	for(var i = 0; i < current_deck.length; i++){
-		console.log(current_deck[i]);
-	}
+	// for(var i = 0; i < current_deck.length; i++){
+	// 	console.log(current_deck[i]);
+	// }
 	return current_deck
 
 }
 
-function dealCards(){
+
+
+function dealCards(curr_deck){
+
+//first we're just going to try to display a card
+
+	var player1_hand = new Array(5);
+
+	//create player hand based off of top 5 of deck
+	for(var i = 0; i < player1_hand.length; i++){
+		player1_hand[i] = curr_deck.pop();
+		document.getElementById('card-image').appendChild(player1_hand[i]);
+	}
+
+
+	curr_deck.splice()
+
+
+
 
 }
 
-initializeDeck();
+
+$(document).ready(function() {
+
+	var deck = initializeDeck();	
+
+	console.log(deck[0]);
+
+	$('#current-deck').on('click', function(){
+		$(this).hide();
+
+		dealCards(deck);
+
+		console.log('spooky button');
+	});
+});
